3dbcac2da1f85fbc4dca3c2fae5bf839c20ba2fd
Max <marc.garcia.u@gmail.com>
2017-05-09 19:58:42 +0200

    som funciont

--- Ex_recursive/Ex_recursive/Functions.h
+++ Ex_recursive/Ex_recursive/Functions.h
@@ -52,3 +52,24 @@ int sumElemsRec(int vec[], int sizeVec, unsigned int index)
 	}
 	return sum;
 }
+
+int fibonacci(int a)
+{
+	int fib = 0;
+	if (a == 0)
+		return 1;
+	else if (a == 1)
+		return 1;
+	else
+		return fibonacci(a-1)+fibonacci(a-2);
+}
+
+int factorial(int num)
+{
+	if (num == 0)
+		return 1;
+	else if (num == 1)
+		return 1;
+	else
+		return num*factorial(num - 1);
+}
--- Ex_recursive/Ex_recursive/main.cpp
+++ Ex_recursive/Ex_recursive/main.cpp
@@ -10,26 +10,42 @@ int main()
 	countUP(10);
 	cout << endl;
 	system("pause");
+	cout << endl;
 
 	cout << "Counter of Trues" << endl;
 	cout << "----------------" << endl;
 	bool nana[10] = { 0,1,1,0,0,1,0,1,0,1 };
 	cout << numTrues(nana, 10, 0) << endl;
-	cout << endl;
 	system("pause");
+	cout << endl;
 
-	cout << "Counter of Trues" << endl;
+	cout << "Serch a number" << endl;
 	cout << "----------------" << endl;
 	int nuko[10] = { 20,50,3,-56,25,4,52,655,87,1};
 	if (serchRec(nuko, 10, 0, 3) == true){cout << "I've founded the number" << endl;}
 	else{cout << "error" << endl;}
+	system("pause");
+	cout << endl;
+
+	cout << "Sum of numbers" << endl;
+	cout << "----------------" << endl;
+	int niko[10] = { 1,2,3,4,5,6,7,8,9,10};
+	cout << sumElemsRec(niko, 10, 0)<<endl;
+	system("pause");
+	cout << endl;
+
+	cout << "Fibonacci" << endl;
+	cout << "----------------" << endl;
+	cout << "first number of the fibonacci secuence: " << fibonacci(0) << endl;
+	cout << "second number of the fibonacci secuence: " << fibonacci(1) << endl;
+	cout << "8th number of the fibonacci secuence: " << fibonacci(16) << endl;
 	cout << endl;
 	system("pause");
 
-	cout << "Counter of Trues" << endl;
+	cout << "Factorial" << endl;
 	cout << "----------------" << endl;
-	int niko[10] = { 1,2,3,4,5,6,7,8,9,10};
-	cout << sumElemsRec(niko, 10, 0);
-	cout << endl;
+	cout << "5! = " << factorial(5)<<endl;
 	system("pause");
+	cout << endl;
+
 }
